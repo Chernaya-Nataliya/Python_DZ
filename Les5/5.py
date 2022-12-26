eaaa77837b7223d1ebd546683eca96396c397443
Nataliya <chernishmail@gmail.com>
2022-12-26 18:38:15 +0300

    Изменения в файле 5.py

--- Les5/5.py
+++ Les5/5.py
@@ -4,9 +4,10 @@ from random import randint
 
 n = int(input("Введите количество записываемых чисел: "))
 numbers = [str(randint(0, 100)) for i in range(n)]
-with open(r'python_dz\les5\les5_5.txt', 'w', encoding='utf-8') as txt:
+print(numbers)
+with open('les5_5.txt', 'w', encoding='utf-8') as txt:
     txt.write(' '.join(numbers))
 
-with open(r'python_dz\les5\les5_5.txt', 'r', encoding='utf-8') as txt:
+with open('les5_5.txt', 'r', encoding='utf-8') as txt:
     content = list(map(int, txt.read().strip().split()))
     print(f'Сумма чисел равна {sum(content)}')
